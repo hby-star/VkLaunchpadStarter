ccbcc372eab603fb4acb5a520acf7db08ff48b21
HouBinyang <2171463703@qq.com>
2025-03-11 18:51:21 +0800

    Assignment 4 enable depth test.

--- src/Main.cpp
+++ src/Main.cpp
@@ -201,6 +201,101 @@ void createBuffer(VkDevice device, VkPhysicalDevice physicalDevice, VkDeviceSize
 	vkBindBufferMemory(device, buffer, bufferMemory, 0);
 }
 
+VkFormat findSupportedFormat(VkPhysicalDevice physicalDevice, const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features)
+{
+	for (VkFormat format : candidates)
+	{
+		VkFormatProperties props;
+		vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &props);
+
+		if (tiling == VK_IMAGE_TILING_LINEAR && (props.linearTilingFeatures & features) == features)
+		{
+			return format;
+		}
+		else if (tiling == VK_IMAGE_TILING_OPTIMAL && (props.optimalTilingFeatures & features) == features)
+		{
+			return format;
+		}
+	}
+
+	throw std::runtime_error("failed to find supported format!");
+}
+
+VkFormat findDepthFormat(VkPhysicalDevice physicalDevice)
+{
+	return findSupportedFormat(physicalDevice,
+		{ VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
+		VK_IMAGE_TILING_OPTIMAL,
+		VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT
+	);
+}
+
+bool hasStencilComponent(VkFormat format)
+{
+	return format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_D24_UNORM_S8_UINT;
+}
+
+VkImageView createImageView(VkDevice device, VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels)
+{
+	VkImageViewCreateInfo viewInfo{};
+	viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+	viewInfo.image = image;
+	viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+	viewInfo.format = format;
+	viewInfo.subresourceRange.aspectMask = aspectFlags;
+	viewInfo.subresourceRange.baseMipLevel = 0;
+	viewInfo.subresourceRange.levelCount = mipLevels;
+	viewInfo.subresourceRange.baseArrayLayer = 0;
+	viewInfo.subresourceRange.layerCount = 1;
+
+	VkImageView imageView;
+	if (vkCreateImageView(device, &viewInfo, nullptr, &imageView) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to create image view!");
+	}
+
+	return imageView;
+}
+
+void createImage(VkDevice device, VkPhysicalDevice physicalDevice, uint32_t width, uint32_t height, uint32_t mipLevels, VkSampleCountFlagBits numSamples, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory)
+{
+	VkImageCreateInfo imageInfo{};
+	imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+	imageInfo.imageType = VK_IMAGE_TYPE_2D;
+	imageInfo.extent.width = width;
+	imageInfo.extent.height = height;
+	imageInfo.extent.depth = 1;
+	imageInfo.mipLevels = mipLevels;
+	imageInfo.arrayLayers = 1;
+	imageInfo.format = format;
+	imageInfo.tiling = tiling;
+	imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	imageInfo.usage = usage;
+	imageInfo.samples = numSamples;
+	imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+	if (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to create image!");
+	}
+
+	VkMemoryRequirements memRequirements;
+	vkGetImageMemoryRequirements(device, image, &memRequirements);
+
+	VkMemoryAllocateInfo allocInfo{};
+	allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize = memRequirements.size;
+	allocInfo.memoryTypeIndex = findMemoryType(physicalDevice, memRequirements.memoryTypeBits, properties);
+
+	if (vkAllocateMemory(device, &allocInfo, nullptr, &imageMemory) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to allocate image memory!");
+	}
+
+	vkBindImageMemory(device, image, imageMemory, 0);
+}
+
+
 struct MVPMatrix
 {
 	glm::mat4 model;
@@ -500,6 +595,16 @@ int main(int argc, char** argv)
 	// Task 1.8: Initialize Vulkan Launchpad
 	/* --------------------------------------------- */
 
+	VkImage depthImage;
+	VkDeviceMemory depthImageMemory;
+	VkImageView depthImageView;
+
+	VkFormat depthFormat = findDepthFormat(vk_physical_device);
+
+	createImage(vk_device, vk_physical_device, swapchain_create_info.imageExtent.width, swapchain_create_info.imageExtent.height, 1, VK_SAMPLE_COUNT_1_BIT, depthFormat, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, depthImage, depthImageMemory);
+	depthImageView = createImageView(vk_device, depthImage, depthFormat, VK_IMAGE_ASPECT_DEPTH_BIT, 1);
+
+
 	// Gather swapchain config as required by the framework:
 	VklSwapchainConfig swapchain_config = {};
 	swapchain_config.imageExtent = swapchain_create_info.imageExtent;
@@ -518,7 +623,10 @@ int main(int argc, char** argv)
 		framebufferData.colorAttachmentImageDetails.clearValue = { 0.0f, 0.0f, 0.0f, 1.0f }; // Clear color to black
 
 		// We don't need the depth attachment now, but keep it in mind for later!
-		framebufferData.depthAttachmentImageDetails.imageHandle = VK_NULL_HANDLE;
+		framebufferData.depthAttachmentImageDetails.imageHandle = depthImage;
+		framebufferData.depthAttachmentImageDetails.imageFormat = depthFormat;
+		framebufferData.depthAttachmentImageDetails.imageUsage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+		framebufferData.depthAttachmentImageDetails.clearValue = { 1.0f, 0 }; // Clear depth to 1.0
 
 		// Add it to the vector:
 		swapchain_config.swapchainImages.push_back(framebufferData);
@@ -709,6 +817,9 @@ int main(int argc, char** argv)
 	model.destroyAllModelBuffers();
 	// Destroy the framework in 1.8
 	vklDestroyFramework();
+	vkDestroyImage(vk_device, depthImage, nullptr);
+	vkFreeMemory(vk_device, depthImageMemory, nullptr);
+	vkDestroyImageView(vk_device, depthImageView, nullptr);
 	// Destroy the swapchain in 1.7
 	vkDestroySwapchainKHR(vk_device, vk_swapchain, nullptr);
 	// Destroy the logical device in 1.6
